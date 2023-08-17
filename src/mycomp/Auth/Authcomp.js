c5127a5030e6390f8cf9cd43b1bbde1bd2fb0acb
MihirModhwadiya <mihirmodhwadiya21@gmail.com>
2023-08-17 19:07:20 +0530

    signup err done with not rmving ref from firebsdoc

--- src/mycomp/Auth/Authcomp.js
+++ src/mycomp/Auth/Authcomp.js
@@ -13,15 +13,31 @@ const Authcomp = () => {
   const [email, setEmail] = useState("");
   const [photoURL, setphotoURL] = useState("");
   const [password, setPassword] = useState("");
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const signUp = async () => {
     try {
+      debugger;
       const res = await createUserWithEmailAndPassword(auth, email, password);
       const storageRef = ref(storage, displayName);
-      const uploadTask = uploadBytesResumable(storageRef, photoURL);
+      const file = photoURL;
+      const uploadTask = uploadBytesResumable(storageRef, file);
 
       uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
         (error) => {
           alert(error.message);
         },
@@ -40,6 +56,7 @@ const Authcomp = () => {
           });
         }
       );
+
       alert("successfully signed up");
     } catch (error) {
       alert(error.message);
